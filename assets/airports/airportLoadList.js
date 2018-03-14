cab68903dfd177738cf92587089f23ac1fea8d1b
Alex Ying <ay9973@gmail.com>
2018-03-14 16:17:53 -0400

    Add KLGA with balanced arrival flow

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -294,6 +294,13 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             name: 'LaGuardia Airport',
             premium: false,
             wip: false
+        },
+        {
+            icao: 'klga_b',
+            level: 'hard',
+            name: 'LaGuardia Airport (Balanced Arrival Flow)',
+            premium: false,
+            wip: false
         // },
         // {
         //     icao: 'kmci',
