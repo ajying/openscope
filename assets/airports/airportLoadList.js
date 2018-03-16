cc6cd54ea2bcaaffd23f61e55cf165ff918b4171
Alex Ying <ay9973@gmail.com>
2018-03-15 20:16:22 -0400

    Replace old KJFK with new KJFK

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -260,17 +260,10 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
         //     premium: false,
         //     wip: false
         // },
-        // {
-        //     icao: 'kjfk',
-        //     level: 'hard',
-        //     name: 'John F Kennedy International Airport',
-        //     premium: false,
-        //     wip: true
-        // },
         {
-            icao: 'kjfk_n',
+            icao: 'kjfk',
             level: 'hard',
-            name: 'John F Kennedy International Airport (New)',
+            name: 'John F Kennedy International Airport',
             premium: false,
             wip: false
         },
